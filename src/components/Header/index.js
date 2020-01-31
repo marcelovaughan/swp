713e754c1bb23445e85007167d6e7713d1d64064
MARCELO FARIA <marcelovaughan@gmail.com>
2020-01-31 03:22:49 -0300

    Basic components

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+    return (        
+        <header>
+            <h1 className="logo">StarWars - Planets</h1>
+            <h2>PLANETS</h2>
+        </header>
+    );
+}
---
+++ src/components/Loader/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+export default function Loader() {
+    return (
+        <div id="spinner">
+            <span className="loader"></span>
+        </div>
+    );
+}
